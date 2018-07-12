0ac83b7e9601a354be1fe5bd50730fdc4cae23a2
pederson <dpederson@utexas.edu>
2018-07-12 13:58:56 -0500

    (added) 3d frame

--- include/Frame.hpp
+++ include/Frame.hpp
@@ -78,5 +78,51 @@ Frame2<SceneType, dim> make_frame_2d(const SceneType & s, Point<dim> start, Poin
 }
 
 
+
+
+
+
+template <typename SceneType, std::size_t scene_dim>
+class Frame3
+{
+private:
+	std::shared_ptr<SceneType> 	mScene;
+	Point<scene_dim> 			mStart;
+	Point<scene_dim>			mEnd;
+	Point<scene_dim>			mVecX;
+	Point<scene_dim> 			mVecY;
+
+	double length_diag() const {return (mEnd-mStart).norm();};
+	Point<scene_dim> vecz() const {return ((mEnd-mStart) - lengthx()*mVecX - lengthy()*mVecY).normalize();};
+public:
+	Frame3(const SceneType & s, Point<scene_dim> start, Point<scene_dim> end, Point<scene_dim> xvec, Point<scene_dim> yvec)
+	: mScene(std::make_shared<SceneType>(s)) 
+	, mStart(start)
+	, mEnd(end)
+	, mVecX(xvec.normalize())
+	, mVecY(yvec.normalize())
+	{};
+
+	decltype(auto) query_point(Point<3> p) const {
+		// std::cout << " xvec: " << mVecX << " yvec: " << vecy() << " ";
+		return mScene->query_point(mStart + p.x[0]*mVecX + p.x[1]*mVecY + p.x[2]*vecz());
+	}
+
+	double lengthx() const {return Point<scene_dim>::dot((mEnd-mStart), mVecX);};
+	double lengthy() const {return Point<scene_dim>::dot((mEnd-mStart), mVecY);};
+	double lengthz() const {return sqrt(length_diag()*length_diag() - lengthx()*lengthx() - lengthy()*lengthy());};
+	
+};
+
+
+
+template <std::size_t dim, typename SceneType>
+Frame3<SceneType, dim> make_frame_3d(const SceneType & s, Point<dim> start, Point<dim> end, Point<dim> xvec, Point<dim> yvec){
+	return Frame3<SceneType, dim>(s, start, end, xvec, yvec);
+}
+
+
+
+
 } // end namespace csg
 #endif
